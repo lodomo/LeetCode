2ba854ae2ca6e7c06d346525f5fffd9925a382cb
Lodomo <lodomo.dev@gmail.com>
2024-12-16 12:56:29 -0800

    0032 idgaf that it takes longer. It works. Success.

--- 0032_-_Longest_Valid_Parens/0032.py
+++ 0032_-_Longest_Valid_Parens/0032.py
@@ -21,66 +21,100 @@ class Solution:
         if newStart >= newEnd:
             return 0
 
-        s = s[newStart:newEnd + 1]
+        s = list(s[newStart: newEnd + 1])
         strlen = len(s)
-        stack = []
-        L = -1
-        R = -2
+
+        # Step 1: Find a () pair there must be atleast 1.
+        for i in range(strlen - 1):
+            if s[i] == "(" and s[i + 1] == ")":
+                s[i] = 1
+                s[i + 1] = 1
+                left = self.pairLeft(i - 1, s, 0)
+                right = self.pairRight(i + 2, s, strlen - 1)
+                self.surrounded(left, right, s, strlen - 1)
+                continue
+
+        # Step 2: Find the longest sequence of 1's
+        maxCount = 0
+        count = 0
         for i in range(strlen):
-            if s[i] == "(":
-                stack.append(L)
-                continue
+            if s[i] == 1:
+                count += 1
+            else:
+                maxCount = max(maxCount, count)
+                count = 0
 
-            if len(stack) == 0:
-                continue
+        print(s)
+        return max(maxCount, count)
 
-            if stack[-1] == L:
-                stack.pop()
-                stack.append(2)
-                continue
+    def pairRight(self, index, parenList, maxIndex):
+        """
+        If the index is a "(" and ")", then turn them into 1's,
+        and keep looking right.
 
-            if stack[-1] == R:
-                stack.append(R)
-                continue
+        if the index is not a pair, then return the index.
+        """
+        if index + 1 > maxIndex:
+            return index
 
-            if len(stack) > 1 and stack[-1] > 0:
-                if stack[-2] == L:
-                    current = stack.pop()
-                    stack.pop()
-                    stack.append(current + 2)
-                    continue
+        if parenList[index] == "(" and parenList[index + 1] == ")":
+            parenList[index] = 1
+            parenList[index + 1] = 1
+            return self.pairRight(index + 2, parenList, maxIndex)
 
-                if stack[-2] == R:
-                    stack.append(R)
-                    continue
+        return index
 
-        print(stack)
+    def pairLeft(self, index, parenList, minIndex):
+        """
+        If the index is a "(" and ")", then turn them into 1's,
+        and keep looking left.
+        """
+        if index - 1 < minIndex:
+            return index
 
-        running_total = 0
-        max_total = 0
-        stack_len = len(stack)
-        for i in range(stack_len):
-            if stack[i] == L or stack[i] == R:
-                max_total = max(max_total, running_total)
-                running_total = 0
-                continue
+        if parenList[index] == ")" and parenList[index - 1] == "(":
+            parenList[index] = 1
+            parenList[index - 1] = 1
+            return self.pairLeft(index - 2, parenList)
 
-            running_total += stack[i]
-        max_total = max(max_total, running_total)
+        return index
 
-        return max_total
+    def surrounded(self, left, right, parenList, maxIndex):
+        """
+        If the left and right are a pair, then turn them into 1's.
+        """
+        while left > 0 and parenList[left] == 1:
+            left -= 1
+
+        while right < maxIndex and parenList[right] == 1:
+            right += 1
+
+        if left < 0 or right > maxIndex:
+            return
+
+        if parenList[left] == "(" and parenList[right] == ")":
+            parenList[left] = 1
+            parenList[right] = 1
+            return self.surrounded(left - 1, right + 1, parenList, maxIndex)
+        return
 
 
 case1 = "(()"
 case2 = ")()())"
 case3 = ""
 case4 = "()(())"
+case5 = ")(((((()())()()))()(()))("
 caseMy = "((((((("
 caseMy2 = ")))))))"
 caseMy3 = "))))))()(((((("
 
+
+"""
 cases = [case1, case2, case3, case4, caseMy, caseMy2, caseMy3]
 
 for case in cases:
     print(f"Running case: {case}")
     print(Solution().longestValidParentheses(case))
+"""
+print(f"Running case: {case5}")
+print(Solution().longestValidParentheses(case5))
